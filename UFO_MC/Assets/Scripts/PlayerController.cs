3dfb027b0ea30922b2a5c68a5d57fa44b73a5acf
Marta Ciekanowska <martaciekanowska27@gmail.com>
2023-04-19 20:08:06 +0200

    ProjectUpdate

--- UFO_MC/Assets/Scripts/PlayerController.cs
+++ UFO_MC/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D rb2d;
 
+    Rigidbody2D rb2d;
+    private int count = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +19,18 @@ public class PlayerController : MonoBehaviour
         float MoveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(MoveHorizontal, MoveVertical);
         rb2d.AddForce(movement * 15);
+
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("PickUp"))
+        {
+            count++;
+            Destroy(collision.gameObject);
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
