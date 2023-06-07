1afe1e3f66abbed035395214c5d2075ff1f98860
Marta Ciekanowska <martaciekanowska27@gmail.com>
2023-06-07 19:18:55 +0200

    ProjectUpdate_UIFinish

--- UFO_MC/Assets/Scripts/AudioManager.cs
+++ UFO_MC/Assets/Scripts/AudioManager.cs
@@ -53,4 +53,24 @@ public class AudioManager : MonoBehaviour
             sfxSource.PlayOneShot(s.audioClip);
         }
     }
+
+    public void ToggleMusic()
+    {
+        musicSource.mute = !musicSource.mute;
+    }
+
+    public void ToggleSFX()
+    {
+        sfxSource.mute = !sfxSource.mute;
+    }
+
+    public void MusicVolume(float volume)
+    {
+        musicSource.volume = volume;
+    }
+
+    public void SFXVolume(float volume)
+    {
+        sfxSource.volume = volume;
+    }
 }
--- UFO_MC/Assets/Scripts/UIController.cs
+++ UFO_MC/Assets/Scripts/UIController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
@@ -16,5 +17,25 @@ public class UIController : MonoBehaviour
         {
             gameObject.SetActive(true);
         }
+    }
+
+    public Slider _musicSlider, _sfxSlider;
+
+    public void ToggleMusic()
+    {
+        AudioManager.instance.ToggleMusic();
+    }
+
+    public void ToggleSFX()
+    {
+        AudioManager.instance.ToggleSFX();
+    }
+    public void MusicVolume()
+    {
+        AudioManager.instance.MusicVolume(_musicSlider.value);
+    }
+    public void SFXVolume()
+    {
+        AudioManager.instance.SFXVolume(_sfxSlider.value);
     }
 }
